90685deb4a7a8a4ed03ac915980ada32ccd5f1e1
gjeck <gregory.jeckell@gmail.com>
2017-08-14 00:32:02 -0700

    Use promise in test

--- tests/bundle.test.js
+++ tests/bundle.test.js
@@ -1,29 +1,38 @@
 const fs = require('fs')
 const archiver = require('archiver')
 
-test('the project is under 13kb', () => {
-  let output = fs.createWriteStream('./dist/build.zip')
-  let archive = archiver('zip', {
-    zlib: { level: 9 }
-  })
+function archive() {
+  return new Promise((resolve, reject) => {
+    let output = fs.createWriteStream('./dist/build.zip')
+    let archive = archiver('zip', {
+      zlib: { level: 9 }
+    })
 
-  output.on('close', function() {
-    expect(archive.pointer()).toBeLessThanOrEqual(13312)
-  })
+    output.on('close', function() {
+      let size = archive.pointer()
+      resolve({ size: size })
+    })
 
-  archive.on('warning', function(err) {
-    if (err.code === 'ENOENT') {
-      console.warn(err)
-    } else {
+    archive.on('warning', function(err) {
+      reject(new Error(err.message))
       throw err
-    }
-  })
+    })
 
-  archive.on('error', function(err) {
-    throw err
-  })
+    archive.on('error', function(err) {
+      reject(new Error(err.message))
+      throw err
+    })
 
-  archive.pipe(output)
-  archive.directory('dist/app/', false)
-  archive.finalize()
+    archive.pipe(output)
+    archive.directory('dist/app/', false)
+    archive.finalize()
+  })
+}
+
+test('the project is under 13kb', () => {
+  expect.assertions(1)
+  return archive().then(data => {
+    expect(data.size).toBeLessThanOrEqual(13312)
+    console.log('Project is utilizing:\n \x1b[33m%s\x1b[0m', data.size + '/13312')
+  })
 })
