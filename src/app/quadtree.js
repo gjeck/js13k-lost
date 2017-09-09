65ea495bf64f2bc6ef009b1ff10bcfda9eac0f5a
gjeck <gregory.jeckell@gmail.com>
2017-09-08 17:27:56 -0700

    Quadtree adjustments

--- src/app/quadtree.js
+++ src/app/quadtree.js
@@ -1,125 +1,123 @@
 import createBoundingRect from './bounding_rect'
 
+function QuadTree(maxItems, depth, bounds) {
+  this.maxItems = maxItems
+  this.depth = depth
+  this.bounds = bounds
+  this.items = []
+  this.nodes = []
+}
+
+QuadTree.prototype.removeAll = function() {
+  this.items.splice(0, this.items.length)
+  this.nodes.forEach((node) => {
+    node.removeAll()
+  })
+  this.nodes.splice(0, this.nodes.length)
+}
+
+QuadTree.prototype.subDivide = function() {
+  const newWidth = Math.floor(this.bounds.width / 2)
+  const newHeight = Math.floor(this.bounds.height / 2)
+  const x = this.bounds.x
+  const y = this.bounds.y
+
+  const northEastTree = createQuadTree({
+    depth: this.depth + 1,
+    x: x + newWidth,
+    y: y,
+    width: newWidth,
+    height: newHeight
+  })
+  this.nodes.push(northEastTree)
+  const northWestTree = createQuadTree({
+    depth: this.depth + 1,
+    x: x,
+    y: y,
+    width: newWidth,
+    height: newHeight
+  })
+  this.nodes.push(northWestTree)
+  const southWestTree = createQuadTree({
+    depth: this.depth + 1,
+    x: x,
+    y: y + newHeight,
+    width: newWidth,
+    height: newHeight
+  })
+  this.nodes.push(southWestTree)
+  const southEastTree = createQuadTree({
+    depth: this.depth + 1,
+    x: x + newWidth,
+    y: y + newHeight,
+    width: newWidth,
+    height: newHeight
+  })
+  this.nodes.push(southEastTree)
+}
+
+QuadTree.prototype.insert = function(item) {
+  if (!this.bounds.contains(item.frame)) {
+    return false
+  }
+
+  if (this.items.length < this.maxItems) {
+    this.items.push(item)
+    return true
+  }
+
+  if (this.nodes.length === 0) {
+    this.subDivide()
+  }
+
+  let addedToChild = false
+  this.nodes.forEach((node) => {
+    if (!addedToChild && node.insert(item)) {
+      addedToChild = true
+    }
+  })
+
+  if (addedToChild) {
+    return true
+  }
+
+  // item is orphaned, and couldn't be added normally
+  this.items.push(item)
+  return true
+}
+
+QuadTree.prototype.query = function(boundingRect) {
+  const results = []
+
+  if (!this.bounds.contains(boundingRect)) {
+    return results
+  }
+
+  this.items.forEach((item) => {
+    if (item.frame.intersects(boundingRect)) {
+      results.push(item)
+    }
+  })
+
+  this.nodes.forEach((node) => {
+    if (node.bounds.intersects(boundingRect)) {
+      const childResults = node.query(boundingRect)
+      childResults.forEach((child) => {
+        results.push(child)
+      })
+    }
+  })
+
+  return results
+}
+
 function createQuadTree(spec) {
   const s = spec || {}
   const maxItems = s.maxItems || 30
   const depth = s.depth || 0
-  const bounds = createBoundingRect(spec)
-  const items = []
-  const nodes = []
+  const bounds = s.bounds || createBoundingRect(s)
 
-  const removeAll = () => {
-    items.splice(0, items.length)
-    nodes.forEach((node) => {
-      node.removeAll()
-    })
-    nodes.splice(0, nodes.length)
-  }
-
-  const subDivide = () => {
-    const newWidth = Math.floor(bounds.width / 2)
-    const newHeight = Math.floor(bounds.height / 2)
-    const x = bounds.x
-    const y = bounds.y
-
-    const northEastTree = createQuadTree({
-      depth: depth + 1,
-      x: x + newWidth,
-      y: y,
-      width: newWidth,
-      height: newHeight
-    })
-    nodes.push(northEastTree)
-    const northWestTree = createQuadTree({
-      depth: depth + 1,
-      x: x,
-      y: y,
-      width: newWidth,
-      height: newHeight
-    })
-    nodes.push(northWestTree)
-    const southWestTree = createQuadTree({
-      depth: depth + 1,
-      x: x,
-      y: y + newHeight,
-      width: newWidth,
-      height: newHeight
-    })
-    nodes.push(southWestTree)
-    const southEastTree = createQuadTree({
-      depth: depth + 1,
-      x: x + newWidth,
-      y: y + newHeight,
-      width: newWidth,
-      height: newHeight
-    })
-    nodes.push(southEastTree)
-  }
-
-  const insert = (item) => {
-    if (!bounds.contains(item.frame)) {
-      return false
-    }
-
-    if (items.length < maxItems) {
-      items.push(item)
-      return true
-    }
-
-    if (nodes.length === 0) {
-      subDivide()
-    }
-
-    let addedToChild = false
-    nodes.forEach((node) => {
-      if (!addedToChild && node.insert(item)) {
-        addedToChild = true
-      }
-    })
-
-    if (addedToChild) {
-      return true
-    }
-
-    // item is orphaned, and couldn't be added normally
-    items.push(item)
-    return true
-  }
-
-  const query = (createBoundingRect) => {
-    const results = []
-
-    if (!bounds.contains(createBoundingRect)) {
-      return results
-    }
-
-    items.forEach((item) => {
-      if (item.frame.intersects(createBoundingRect)) {
-        results.push(item)
-      }
-    })
-
-    nodes.forEach((node) => {
-      if (node.bounds.intersects(createBoundingRect)) {
-        const childResults = node.query(createBoundingRect)
-        childResults.forEach((child) => {
-          results.push(child)
-        })
-      }
-    })
-
-    return results
-  }
-
-  return {
-    depth: depth,
-    bounds: bounds,
-    removeAll: removeAll,
-    insert: insert,
-    query: query,
-    items: items,
-    nodes: nodes
-  }
+  return new QuadTree(maxItems, depth, bounds)
 }
 
 export { createQuadTree as default }
